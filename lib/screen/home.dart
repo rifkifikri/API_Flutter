a72ca3970833fcd2abcd1e739e5b8227c754350f
rifkifikri <rifkifikri000@gmail.com>
2022-11-29 11:21:38 +0700

    tinggal legth

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -26,9 +26,10 @@ Future <Map<String,dynamic>?> getDataUser() async{
       print('user not found');
       return null;
     }else{
-      
-       // print(datas['data'][1]['email']);
+         //var datas =json.decode(response.body) as Map<String,dynamic>;
+         //print(datas['data'][1]['email']);
       return json.decode(response.body) as Map<String,dynamic> ;
+      //return response.body;
     }
   }
 
@@ -61,18 +62,76 @@ Future <Map<String,dynamic>?> getDataUser() async{
           ],
         ),
       ),
-      body: FutureBuilder (
+      body: FutureBuilder <Map<String,dynamic>?>(
         future: getDataUser(),
-        builder: (_, snapshot) {
-        return ListView.builder(
-                    itemCount: 3,
+        builder: (context, snapshot) {
+          return ListView.builder(
+                    itemCount: 5,
                     itemBuilder: (_, index) {
                       var map = snapshot;
                           print(map);
-                      return Text('email');
-                    });
+                      return SizedBox(
+                        child: GestureDetector(
+                          onTap: () => {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) =>MyProfil(),))
+                          },
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(MediaQuery.of(context).size.width * 0.03),
+                          ),
+                          elevation: 5,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: MediaQuery.of(context).size.width * 0.02,
+                                    right: MediaQuery.of(context).size.width  * 0.02,
+                                    top: MediaQuery.of(context).size.width  * 0.02,
+                                    bottom: MediaQuery.of(context).size.width  * 0.02),
+                                child: Container(),
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: CircleAvatar(
+                                      backgroundImage: NetworkImage(snapshot.data!['data'][index]['avatar']),
+                                    ),
+                                  ),
+                                  Text("${snapshot.data!['data'][index]['first_name']}"),
+                                ],
+                              ),
+                              Padding(
+                              padding: EdgeInsets.only(
+                                left: MediaQuery.of(context).size.width * 0.02,
+                                right: MediaQuery.of(context).size.width * 0.02,
+                                top: MediaQuery.of(context).size.width * 0.02,
+                                bottom: MediaQuery.of(context).size.width * 0.02),
+                                child: Container(),
+                              ),
+                            ],
+                          ),
+                          ),
+                        ),
+                      );
+                      
+                      //Text("${snapshot.data!['data'][index]['email']}");
+                     });
+          
+          //Text("${snapshot.data!['data'][1]['email']}");
+        },
+      //   builder: (_, snapshot) {
+      //   return ListView.builder(
+      //               itemCount: 3,
+      //               itemBuilder: (_, index) {
+      //                 var map = snapshot;
+      //                     print(map);
+      //                 return Text('email');
+      //               });
 
-      },),
+      // },
+      ),
       // body: ListView.builder(
       //    itemCount: 1,
       //   // //itemCount: items.length,
@@ -83,51 +142,7 @@ Future <Map<String,dynamic>?> getDataUser() async{
       //   //   );
       //   // },
       //   itemBuilder: (context, index) {
-      //     return SizedBox(
-      //       child: GestureDetector(
-      //         onTap: () => {
-      //           Navigator.push(context, MaterialPageRoute(builder: (context) =>MyProfil(),))
-      //         },
-      //         child: Card(
-      //           shape: RoundedRectangleBorder(
-      //           borderRadius:
-      //               BorderRadius.circular(MediaQuery.of(context).size.width * 0.03),
-      //         ),
-      //         elevation: 5,
-      //         child: Column(
-      //           children: [
-      //             Padding(
-      //               padding: EdgeInsets.only(
-      //                   left: MediaQuery.of(context).size.width * 0.02,
-      //                   right: MediaQuery.of(context).size.width  * 0.02,
-      //                   top: MediaQuery.of(context).size.width  * 0.02,
-      //                   bottom: MediaQuery.of(context).size.width  * 0.02),
-      //               child: Container(),
-      //             ),
-      //             Row(
-      //               children: [
-      //                 Padding(
-      //                   padding: const EdgeInsets.all(8.0),
-      //                   child: CircleAvatar(
-                          
-      //                   ),
-      //                 ),
-      //                 Text('halo'),
-      //               ],
-      //             ),
-      //             Padding(
-      //             padding: EdgeInsets.only(
-      //               left: MediaQuery.of(context).size.width * 0.02,
-      //               right: MediaQuery.of(context).size.width * 0.02,
-      //               top: MediaQuery.of(context).size.width * 0.02,
-      //               bottom: MediaQuery.of(context).size.width * 0.02),
-      //               child: Container(),
-      //             ),
-      //           ],
-      //         ),
-      //         ),
-      //       ),
-      //     );
+      //     return 
       //   },
       // ),
     );
